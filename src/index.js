b516eace961b5438d992497b4f6c63b9ce5e5def
jyotpatel14 <jyotkpatel14@gmail.com>
2025-05-13 12:41:16 +0530

    Minor Updates

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("LMS Backend API is running");
+  res.send("Message Update");
 });
 
 app.listen(PORT, () => {
