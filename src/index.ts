255305807e9c8da1b6eb834966a80d31b16429b7
jyotpatel14 <jyotkpatel14@gmail.com>
2025-05-13 13:14:37 +0530

    Updates with git ignore

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello from TypeScript + Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
